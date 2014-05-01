c67e732981948960f1b4ed588413950e680d0a76
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-05-01 11:33:35 -0700

    Initial payments guide
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- make.sh
+++ make.sh
@@ -7,5 +7,6 @@ BUILD_DIR=$DIR/build
 
 rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
 asciidoctor -B $USERGUIDE_INPUT_DIR -D $BUILD_DIR $USERGUIDE_INPUT_DIR/userguide.adoc
+asciidoctor -B $USERGUIDE_INPUT_DIR -D $BUILD_DIR $USERGUIDE_INPUT_DIR/payments.adoc
 asciidoctor -B $FAQ_INPUT_DIR -D $BUILD_DIR $FAQ_INPUT_DIR/faq.adoc
-echo "open $BUILD_DIR/userguide.html $BUILD_DIR/faq.html"
+echo "open $BUILD_DIR/userguide.html $BUILD_DIR/faq.html $BUILD_DIR/payments.html"
