57ae090bdb0bc456b43458e9fd1066ed2d9ae6fc
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-10-28 08:08:57 -0400

    userguide: setup build for WP
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- make.sh
+++ make.sh
@@ -1,14 +1,43 @@
 #!/bin/bash -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-USERGUIDE_INPUT_DIR=$DIR/userguide
-FAQ_INPUT_DIR=$DIR/faq
-BUILD_DIR=$DIR/build
 
+# Actual content
+USERGUIDE_INPUT_DIR=$DIR/userguide
+# WordPress layout, per section
+WP_INPUT_DIR=$USERGUIDE_INPUT_DIR/layout/wp
+SELFCONTAINED_INPUT_DIR=$USERGUIDE_INPUT_DIR/layout/selfcontained
+# Common options
+COMMON_OPTS="-a sourcedir=$USERGUIDE_INPUT_DIR,imagesdir=$USERGUIDE_INPUT_DIR/assets"
+# Special options for WordPress
+WP_EXTRA_OPTS="$COMMON_OPTS -a data-uri,stylesheet! -s"
+# Selfcontained options
+SELFCONTAINED_EXTRA_OPTS="$COMMON_OPTS -a doctype=book,toc,toclevels=6,data-uri,linkcss!,source-highlighter=highlightjs,homepage=http://killbill.io"
+
+BUILD_DIR=$DIR/build
+WP_BUILD_DIR=$BUILD_DIR/wp
+SELFCONTAINED_BUILD_DIR=$BUILD_DIR/selfcontained
+
+WP_BUILD="asciidoctor $WP_EXTRA_OPTS -B $USERGUIDE_INPUT_DIR -D $WP_BUILD_DIR"
+SELFCONTAINED_BUILD="asciidoctor $SELFCONTAINED_EXTRA_OPTS -B $USERGUIDE_INPUT_DIR -D $SELFCONTAINED_BUILD_DIR"
+
+# Setup
 rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
-asciidoctor -B $USERGUIDE_INPUT_DIR -D $BUILD_DIR $USERGUIDE_INPUT_DIR/userguide_platform.adoc
-asciidoctor -B $USERGUIDE_INPUT_DIR -D $BUILD_DIR $USERGUIDE_INPUT_DIR/userguide_subscription.adoc
-asciidoctor -B $USERGUIDE_INPUT_DIR -D $BUILD_DIR $USERGUIDE_INPUT_DIR/userguide_payment.adoc
-asciidoctor -B $FAQ_INPUT_DIR -D $BUILD_DIR $FAQ_INPUT_DIR/faq.adoc
-#asciidoctor -B $USERGUIDE_INPUT_DIR -D $BUILD_DIR $USERGUIDE_INPUT_DIR/bitcoin.adoc
-echo "open $BUILD_DIR/userguide_payment.html $BUILD_DIR/userguide_platform.html $BUILD_DIR/userguide_subscription.html $BUILD_DIR/faq.html"
+
+echo "*** Building WordPress sections"
+for doc in `find $WP_INPUT_DIR -type d -maxdepth 1`; do
+  for section in `find $doc -type f -maxdepth 1`; do
+    echo "Building $section"
+    $WP_BUILD $section
+  done
+done
+for doc in `find $WP_INPUT_DIR -type f -maxdepth 1`; do
+  echo "Building $doc"
+  $WP_BUILD $doc
+done
+
+echo "*** Building standalone guides"
+for doc in `find $SELFCONTAINED_INPUT_DIR -type f -maxdepth 1`; do
+  echo "Building $doc"
+  $SELFCONTAINED_BUILD $doc
+done
