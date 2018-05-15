38be88824325cd70b3207a99790f6a2ef21bf892
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-05-15 09:45:19 +0100

    Getting started iteration
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -3,7 +3,7 @@
 GH_REF=github.com/killbill/killbill-docs.git
 BUILD=`mktemp -d "${TMPDIR:-/tmp}"/foo.XXXX`
 
-cp -r build $BUILD
+cp -r build index.html $BUILD
 
 pushd $BUILD
 git clone --depth=5 --branch=gh-pages git://$GH_REF
@@ -14,7 +14,7 @@ popd
 #
 VERSION=0.19
 mkdir -p $BUILD/killbill-docs/$VERSION
-cp -f $BUILD/build/selfcontained/* $BUILD/killbill-docs/$VERSION/
+cp -f $BUILD/index.html $BUILD/build/selfcontained/* $BUILD/killbill-docs/$VERSION/
 
 pushd $BUILD/killbill-docs
 git config user.name "Travis-CI"
