189f3c6337109b9ff20f1125b29c9c5381646fa3
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-07-12 10:39:31 +0100

    Update build script for 0.20
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -9,17 +9,17 @@ pushd $BUILD
 git clone --depth=5 --branch=gh-pages git://$GH_REF
 popd
 
-#
-# /!\ Script specific to 0.19 /!\
-#
-VERSION=0.19
+VERSION=$(cat $BUILD/killbill-docs/latest.txt)
 mkdir -p $BUILD/killbill-docs/$VERSION
+mkdir -p $BUILD/killbill-docs/latest
 cp -f $BUILD/index.html $BUILD/build/selfcontained/* $BUILD/killbill-docs/$VERSION/
+cp -f $BUILD/build/selfcontained/* $BUILD/killbill-docs/latest/
 
 pushd $BUILD/killbill-docs
 git config user.name "Travis-CI"
 git config user.email "travis@killbill.io"
 git add $VERSION
+git add latest
 git commit -m "Docs update"
 git push -fq "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages > /dev/null 2>&1
 popd
