4f21c04063647c08c8baf3364d633029fa07c41a
Pierre-Alexandre Meyer <pierre@mouraf.org>
2019-01-02 08:09:30 +0000

    0.20: update ddl.sql
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- 0.20/ddl.sql
+++ 0.20/ddl.sql
@@ -403,6 +403,24 @@ CREATE INDEX blocking_states_tenant_account_record_id ON blocking_states(tenant_
 
 /*! SET default_storage_engine=INNODB */;
 
+DROP TABLE IF EXISTS invoice_tracking_ids;
+CREATE TABLE invoice_tracking_ids (
+    record_id serial unique,
+    id varchar(36) NOT NULL,
+    tracking_id varchar(128) NOT NULL,
+    invoice_id varchar(36) NOT NULL,
+    subscription_id varchar(36),
+    unit_type varchar(255) NOT NULL,
+    record_date date NOT NULL,
+    created_by varchar(50) NOT NULL,
+    created_date datetime NOT NULL,
+    account_record_id bigint /*! unsigned */ not null,
+    tenant_record_id bigint /*! unsigned */ not null default 0,
+    PRIMARY KEY(record_id)
+) /*! CHARACTER SET utf8 COLLATE utf8_bin */;
+CREATE INDEX invoice_tracking_tenant_account_date_idx ON invoice_tracking_ids(tenant_record_id, account_record_id, record_date);
+
+
 DROP TABLE IF EXISTS invoice_items;
 CREATE TABLE invoice_items (
     record_id serial unique,
@@ -1114,7 +1132,7 @@ CREATE TABLE tenant_kvs (
    updated_by varchar(50) DEFAULT NULL,
    PRIMARY KEY(record_id)
 ) /*! CHARACTER SET utf8 COLLATE utf8_bin */;
-CREATE INDEX tenant_kvs_key ON tenant_kvs(tenant_key);
+CREATE INDEX tenant_kvs_trid_key ON tenant_kvs(tenant_record_id, tenant_key);
 
 
 DROP TABLE IF EXISTS tenant_broadcasts;
