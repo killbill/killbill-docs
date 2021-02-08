e4fcb11d957998f6be67d6ba62df9aed3ad9c74a
Nekores <sajidxullah@gmail.com>
2021-02-08 15:48:07 +0500

    Updated v3 with killbill repo v3

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -3,7 +3,7 @@
 GH_REF=github.com/killbill/killbill-docs.git
 BUILD=`mktemp -d "${TMPDIR:-/tmp}"/foo.XXXX`
 
-cp -r build index.html $BUILD
+cp -r build stylesheets javascripts index.html $BUILD
 
 pushd $BUILD
 git clone --depth=5 --branch=gh-pages git://$GH_REF
@@ -12,6 +12,9 @@ popd
 VERSION=$(cat $BUILD/killbill-docs/latest.txt)
 mkdir -p $BUILD/killbill-docs/$VERSION
 mkdir -p $BUILD/killbill-docs/latest
+cp -f $BUILD/stylesheets/* $BUILD/killbill-docs/stylesheets/
+cp -f $BUILD/javascripts/* $BUILD/killbill-docs/javascripts/
+cp -f $BUILD/index.html $BUILD/killbill-docs/
 cp -f $BUILD/index.html $BUILD/build/selfcontained/* $BUILD/killbill-docs/$VERSION/
 # This will also copy the manually generated files (*.xsd, ddl.sql)
 cp -f $BUILD/killbill-docs/$VERSION/* $BUILD/killbill-docs/latest/
@@ -19,8 +22,7 @@ cp -f $BUILD/killbill-docs/$VERSION/* $BUILD/killbill-docs/latest/
 pushd $BUILD/killbill-docs
 git config user.name "Kill Bill core team"
 git config user.email "contact@killbill.io"
-git add $VERSION
-git add latest
+git add $VERSION latest stylesheets javascripts index.html
 git commit -m "Docs update"
 git push -f "https://${GH_TOKEN}:x-oauth-basic@${GH_REF}" gh-pages:gh-pages
 popd
