7fae382dd8287bc4ef4159747875d623c10573cc
Pierre-Alexandre Meyer <pierre@mouraf.org>
2022-01-07 18:28:24 +0000

    Prepare for deployment
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -9,15 +9,18 @@ pushd $BUILD
 git clone --depth=5 --branch=gh-pages git://$GH_REF
 popd
 
-VERSION=0.24
+VERSION=$(cat $BUILD/killbill-docs/latest.txt)
 mkdir -p $BUILD/killbill-docs/$VERSION
+mkdir -p $BUILD/killbill-docs/latest
+cp -f $BUILD/build/selfcontained/index.html $BUILD/killbill-docs/
 cp -f $BUILD/build/selfcontained/* $BUILD/killbill-docs/$VERSION/
-
+# This will also copy the manually generated files (*.xsd, ddl.sql)
+cp -f $BUILD/killbill-docs/$VERSION/* $BUILD/killbill-docs/latest/
 
 pushd $BUILD/killbill-docs
 git config user.name "Kill Bill core team"
 git config user.email "contact@killbill.io"
-git add $VERSION
-git commit -m "Docs update (redesign)"
+git add $VERSION latest index.html
+git commit -m "Docs update"
 git push -f "https://${GH_TOKEN}:x-oauth-basic@${GH_REF}" gh-pages:gh-pages
 popd
