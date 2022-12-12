8501b9c029840d89660de64ce7baa616105811ff
reshmabidikar <reshmabidikar@outlook.com>
2022-12-12 14:12:31 +0530

    Deprecated 0.22 docs

--- deprecate_version.sh
+++ deprecate_version.sh
@@ -4,10 +4,17 @@ then
   exit 1
 fi
 
+echo "Reshma 1"
+
 DEPRECATED_CODE="<div style=\"background-color: hsl(353, 100%, 95%); color: #ff0000; position: sticky; top: 0; padding-top: 20px; padding-bottom: 20px; \
 text-align: center; z-index: 10;\"><strong>This version of Kill Bill is not supported anymore, please see our \
 <a href=\"https://docs.killbill.io\">latest documentation</a></strong></div>"
 
+echo "Reshma DEPRECATED_CODE $DEPRECATED_CODE"
+
 DEPRECATED_CODE_ESCAPED=$(sed 's/[&/\]/\\&/g' <<<"$DEPRECATED_CODE")
+
+echo "Reshma DEPRECATED_CODE_ESCAPED $DEPRECATED_CODE_ESCAPED"
+
 
 sed -i -e "s/<body.*class=\"book\">/&\n$DEPRECATED_CODE_ESCAPED/g" ./$1/*.html
