65caaa8b5e49e34911797a1b4834ac7e99a7fa04
Pierre-Alexandre Meyer <pierre@mouraf.org>
2023-08-28 20:50:33 +0100

    Update cloudflare_deploy.sh

--- cloudflare_deploy.sh
+++ cloudflare_deploy.sh
@@ -13,5 +13,8 @@ else
     mv * $ALL_BUILT_FILES_TMP_DIR/
 
     mkdir -p build/latest
-    mv $ALL_BUILT_FILES_TMP_DIR/* build/latest/
+    mv $ALL_BUILT_FILES_TMP_DIR/* build/
+    # For relative links
+    cp -r build/javascripts build/latest/
+    cp -r build/stylesheets build/latest/
 fi
